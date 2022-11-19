97ef10542cc0ae688b69e3a93d5ce981ea01196c
Gabriel <gabriel22t@hotmail.com>
2022-11-19 17:44:39 +0100

    Borrados los metodos del cliente

--- ClientGroupServer/src/centralizedusergroups/ClientGroupServer.java
+++ ClientGroupServer/src/centralizedusergroups/ClientGroupServer.java
@@ -25,9 +25,7 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ClientGroupServer implements GroupServerInterface {
 
-    private HashMap<String,Group> listaGrupos;
-    private HashMap<String,Condition> condiciones;
-    private HashMap<String,Boolean> booleanos; 
+   
     
     public Scanner scan;
     public String alias;
@@ -41,10 +39,7 @@ public class ClientGroupServer implements GroupServerInterface {
     
     public ClientGroupServer() throws UnknownHostException{
         super();
-        this.condiciones = new HashMap();
-        this.booleanos = new HashMap();
-        this.cerrojo = new ReentrantLock(true);
-        this.listaGrupos = new HashMap();
+        
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
@@ -173,189 +168,62 @@ public class ClientGroupServer implements GroupServerInterface {
     }
 
     @Override
-    public boolean createGroup(String galias, String oalias, String ohostname) throws RemoteException{
-        
-        if (!this.isGroup(galias)){
-            this.cerrojo.lock();
-            this.listaGrupos.put(galias,new Group(galias,new GroupMember(oalias,ohostname)));
-            this.condiciones.put(galias,this.cerrojo.newCondition());
-            this.booleanos.put(galias,false);
-            this.cerrojo.unlock();
-            return true;
-        }else return false;
-        
-        
+    public boolean createGroup(String galias, String oalias, String ohostname) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean isGroup(String galias) throws RemoteException{
-        
-        if (this.listaGrupos.containsKey(galias)){
-        return true;
-        }else return false;
-              
-        
+    public boolean isGroup(String galias) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean removeGroup(String galias, String oalias) throws RemoteException {
-        
-        if(this.isGroup(galias) && this.listaGrupos.get(galias).propietario.equals(oalias)){//Esto funciona porque java cortocicuita la condicion
-            this.cerrojo.lock();
-            this.listaGrupos.remove(galias);
-            this.cerrojo.unlock();
-            return true;
-        }else return false;
-        
-            
-        
-        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean addMember(String galias, String alias, String hostname) throws RemoteException {
-        
-        
-        if(this.isGroup(galias) && this.isMember(galias, alias)){
-            this.cerrojo.lock();
-            this.listaGrupos.get(galias).listaMiembros.add(new GroupMember(alias,hostname));
-            this.cerrojo.unlock();
-            return true;
-        }else return false;
-           
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean removeMember(String galias, String alias) throws RemoteException {
-        if(this.booleanos.get(galias)){
-            try {
-                this.condiciones.get(galias).await();//Si los miembros del grupo estan parados me espero a que vuelvan a estar permitidos
-            } catch (InterruptedException ex) {
-                System.out.println("Ha ocurrido una excepcion en el await de removeMember");
-            }
-        }
-        
-        if(this.isMember(galias,alias)){
-            return this.listaGrupos.get(galias).removeMember(alias);
-        }else{
-            return false;
-        }    
-        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean isMember(String galias, String alias) throws RemoteException {
-        this.cerrojo.lock();
-        try{
-            boolean devolver = false;
-            if(this.isGroup(galias)){
-            for (GroupMember miembro :this.listaGrupos.get(galias).listaMiembros){
-                if (miembro.nombre.equals(alias)){
-                    devolver = true;
-                }
-            }
-            }
-        return devolver;
-        }finally{
-            this.cerrojo.unlock();
-        }
-        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public String Owner(String galias) throws RemoteException {
-        this.cerrojo.lock();
-        try{
-            if (this.isGroup(galias)){
-            return this.listaGrupos.get(galias).propietario.nombre;
-        }else return null;
-        }finally{
-            this.cerrojo.unlock();
-        }
-        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean StopMembers(String galias) throws RemoteException {
-        if(this.isGroup(galias)){
-            this.cerrojo.lock();
-            this.booleanos.put(galias,true);//cambio el valor del booleano del grupo
-            this.cerrojo.unlock();
-            return true;
-        }else{
-            return false;
-        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean AllowMembers(String galias) throws RemoteException {
-        if(this.isGroup(galias)){
-            this.cerrojo.lock();
-            this.booleanos.put(galias,false);//cambio el valor del booleano del grupo
-            this.condiciones.get(galias).signalAll();
-            this.cerrojo.unlock();
-            return true;
-        }else{
-            return false;
-        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public LinkedList<String> ListMembers(String galias) {
-        LinkedList<String> devolver = new LinkedList<>();
-        Group grupo = this.listaGrupos.get(galias);
-        for (GroupMember miembro:grupo.listaMiembros){
-            devolver.add(miembro.nombre);
-        }
-        return devolver;
+    public LinkedList<String> ListMembers(String galias) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public LinkedList<String> ListGroups() {
-        LinkedList devolver = new LinkedList();
-        for (String key:this.listaGrupos.keySet()){
-            devolver.add(key);
-        }
-            
-        
-        return devolver;
+    public LinkedList<String> ListGroups() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-}
-class Group{
-    
-    String nombreGrupo;
-    
-    GroupMember propietario;
-    
-    LinkedList<GroupMember> listaMiembros;
+
     
     
-    public Group(String nombreGrupo, GroupMember groupMember){
-        this.listaMiembros = new LinkedList();
-        this.nombreGrupo = nombreGrupo;
-        this.propietario = groupMember;
-        this.listaMiembros.add(groupMember);
-    }
-    
-    public String getNombreGrupo(){
-        return this.nombreGrupo;
-    }
-    
-    //Funcion para eliminar un miembro con alias
-    public boolean removeMember(String alias){
-        if (this.propietario.nombre.equals(alias)){ // Comprobamos que no es el propietario
-            return false;
-        }else{
-            for(GroupMember miembro:listaMiembros){
-                if(miembro.nombre.equals(alias)){
-                    this.listaMiembros.remove(miembro);
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 }
     
 
